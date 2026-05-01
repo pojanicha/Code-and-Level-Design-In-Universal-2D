9302a3c03cf03bd372fccc062cb75de4aba3a88d
pojanicha <pojanicha.sora@bumail.net>
2026-05-02 05:24:41 +0700

    LIght

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,12 +4,27 @@ public class Checkpoint : MonoBehaviour
 {
     public Collider2D trigger;
 
+    public Sprite normalSprite;
+    public Sprite activelSprite;
+
+    private SpriteRenderer sr;
+
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = normalSprite;
+    }
+
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             RespawnController.Instance.respawnPoint = transform;
+            sr.sprite = activelSprite;
             trigger.enabled = false;
         }
     }
